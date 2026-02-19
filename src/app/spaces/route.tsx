e1871670029571a9f10635bed4662983986df8ed
corn-snake <snakecorn@proton.me>
2026-02-19 00:38:42 +0000

    two search criteria, debounce with typeds

--- src/app/spaces/route.tsx
+++ src/app/spaces/route.tsx
@@ -5,9 +5,12 @@ const PAGINATION_SIZE = 5;
 
 export const GET = async (req: NextRequest) => {
     const page = parseInt(req.nextUrl.searchParams.get("page") || "0");
-    const type = parseInt(req.nextUrl.searchParams.get("type") || "*");
-    const cupo = parseInt(req.nextUrl.searchParams.get("cupo") || "*");
+    const type = req.nextUrl.searchParams.get("type") || "";
+    const cupo = parseInt(req.nextUrl.searchParams.get("cupo") || "0");
     return new NextResponse(JSON.stringify(
-        (await sb.from("sala_data").select().range(page * PAGINATION_SIZE, (page + 1) * PAGINATION_SIZE - 1).order("type", {ascending: false}).order("number",{ascending: true})).data
+        (await sb.from("sala_data").select().range(page * PAGINATION_SIZE, (page + 1) * PAGINATION_SIZE - 1)
+            .ilike("type", `%${type}%`)
+            .gte("cupo", cupo)
+            .order("type", {ascending: false}).order("number",{ascending: true})).data
     ))
 };
--- src/components/med/spacecollection.tsx
+++ src/components/med/spacecollection.tsx
@@ -6,18 +6,38 @@ import Space from "@/components/min/space";
 import { Button } from "../min/legacygeneric";
 
 const SpaceCollection: React.FC = () => {
-    const { spaces, setSpaces } = useSpace();
+    const { spaces, setSpaces, type, cupo } = useSpace();
     const [page, setPage] = useState(1);
     const [loadable, setLoadable] = useState(false);
+    const [timeout, setTimeoutNum] = useState<number | null>(null);
     useEffect(() => { fetch(ENDPOINTS.getSpaces).then(r => r.json()).then((sps: SpaceParams[] | null)=>{
         if (sps && sps.length > 4) setLoadable(true);
         setSpaces(sps);
     }) }, []);
+    const wipeThenSearch = () => {
+        setSpaces(null);
+        const url = new URL(window.location.protocol + "//" + window.location.host);
+        url.pathname = ENDPOINTS.getSpaces;
+        if (type) url.searchParams.append("type", type);
+        if (cupo) url.searchParams.append("cupo", cupo);
+        fetch(url.toString()).then(r => r.json()).then((sps: SpaceParams[] | null) => {
+            if (sps && sps.length > 4) setLoadable(true);
+            setSpaces(sps);
+        });
+    };
+    useEffect(()=>{
+        if(timeout) window.clearTimeout(timeout);
+        setTimeoutNum(window.setTimeout(wipeThenSearch, 500));
+    }, [type]);
+    useEffect(()=>{
+        if(timeout) window.clearTimeout(timeout);
+        wipeThenSearch();
+    }, [cupo])
     if (!spaces) return null;
     const renderGalaxy = ()=>spaces.map((space, i) => <Space space={space} key={i} />);
     const loadMore = () => {
         setPage(prev=>prev + 1);
-        fetch(ENDPOINTS.getSpaces + "/?page=" + page).then(r => r.json()).then((s: SpaceParams[] | null) =>{
+        fetch(`${ENDPOINTS.getSpaces}/?page=${page}${type ? "&type=" + type : ""}${cupo ? "&cupo=" + cupo : ""}`).then(r => r.json()).then((s: SpaceParams[] | null) =>{
             if (!s || s.length < 4) return setLoadable(false);
             setSpaces(() => spaces.concat(s));
         });
