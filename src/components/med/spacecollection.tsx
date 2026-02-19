6c6ea89af67099d7bbf7789eba823f613ac969fa
corn-snake <snakecorn@proton.me>
2026-02-19 02:33:08 +0000

    HOTFIX: loading spaces and loading hint. waow

--- src/components/med/spacecollection.tsx
+++ src/components/med/spacecollection.tsx
@@ -10,15 +10,21 @@ const SpaceCollection: React.FC = () => {
     const [page, setPage] = useState(1);
     const [loadable, setLoadable] = useState(true);
     const [timeout, setTimeoutNum] = useState<number | null>(null);
+    const [loading, setLoading] = useState<number>(1);
     const wipeThenSearch = () => {
         const url = new URL(window.location.protocol + "//" + window.location.host);
         url.pathname = ENDPOINTS.getSpaces;
         if (page > 1) url.searchParams.append("page", `${page}`);
         if (type) url.searchParams.append("type", type);
         if (cupo) url.searchParams.append("cupo", cupo);
+        setLoading(1);
         fetch(url.toString()).then(r => r.json()).then((sps: SpaceParams[] | null) => {
             if (sps && sps.length > 4) setLoadable(false);
             setSpaces(sps);
+            setLoading(0);
+        }).catch(e=>{
+            setLoading(-1);
+            console.error(e);
         });
     };
     useEffect(()=>{
@@ -26,13 +32,20 @@ const SpaceCollection: React.FC = () => {
         setPage(1);
         setTimeoutNum(window.setTimeout(()=>wipeThenSearch(), 500));
     }, [type, cupo]);
-    if (!spaces) return null;
+    if (loading === 1) return <h4 className="italic text-slate-500 w-full text-center">Cargando...</h4>;
+    if (loading < 0 || loading > 1) return <h4 className="text-red-700">you've met a terrible fate, haven't you?</h4>;
+    if (!spaces) return <h4 className="italic text-slate-900 w-full text-center">Sin resultados!</h4>;
     const renderGalaxy = ()=>spaces.map((space, i) => <Space space={space} key={i} />);
     const loadMore = React.cache(() => {
         setPage(prev=>prev + 1);
+        setLoading(1);
         fetch(`${ENDPOINTS.getSpaces}/?page=${page}${type ? "&type=" + type : ""}${cupo ? "&cupo=" + cupo : ""}`).then(r => r.json()).then((s: SpaceParams[] | null) =>{
             if (!s || s.length < 4) return setLoadable(false);
             setSpaces(() => spaces.concat(s));
+            setLoading(0);
+        }).catch(e=>{
+            setLoading(-1);
+            console.error(e);
         });
     });
     return loadable ? renderGalaxy().concat(<Button onClick={loadMore} key={999999}>Cargar más...</Button>) : renderGalaxy();
